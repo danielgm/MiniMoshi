fbdea93471c1a1c87f0b3c644cbe183b5f3da2a1
Niel McLaren <d@nielmclaren.com>
2016-02-19 21:46:34 -0800

    Vertical slices.

--- MiniMoshiApp/MiniMoshiApp.ino
+++ MiniMoshiApp/MiniMoshiApp.ino
@@ -24,6 +24,7 @@ void setup() {
 #define WHITE  0xFFFFFF
 
 int sliceLen = 9;
+
 int numHSlices = 4;
 int hSlices[4][9] {
   {0, 7, 8, 11, 12, 18, 19, -1, -1},
@@ -32,13 +33,24 @@ int hSlices[4][9] {
   {3, 4, -1, -1, -1, -1, -1, -1, -1},
 };
 
+int numVSlices = 6;
+int vSlices[6][9] {
+  {0, 1, -1, -1, -1, -1, -1, -1, -1},
+  {2, 3, 4, 5, 6, -1, -1, -1, -1},
+  {7, 8, 9, 10, 11, 12, 13, -1, -1},
+  {14, 15, 16, 17, -1, -1, -1, -1, -1},
+  {18, 19, 22, 23, -1, -1, -1, -1, -1},
+  {20, 21, -1, -1, -1, -1, -1, -1, -1}
+};
+
+
 int currLED = 0;
 int currSlice = 0;
 
 void loop() {
   colorWipe(WHITE);
   for (int i = 0; i < sliceLen; i++) {
-    int index = hSlices[currSlice][i];
+    int index = vSlices[currSlice][i];
     if (index >= 0) {
       leds.setPixel(index, RED);
     }
@@ -47,7 +59,7 @@ void loop() {
   delayMicroseconds(2000000);
 
   currSlice++;
-  if (currSlice >= numHSlices) {
+  if (currSlice >= numVSlices) {
     currSlice = 0;
   }
 }
