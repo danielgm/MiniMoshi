6d699e7a30ef3d988418f38fd47d8ebc0d90cf6d
Niel McLaren <d@nielmclaren.com>
2016-02-19 21:46:20 -0800

    Horizontal slices.

--- MiniMoshiApp/MiniMoshiApp.ino
+++ MiniMoshiApp/MiniMoshiApp.ino
@@ -23,34 +23,37 @@ void setup() {
 #define ORANGE 0xE05800
 #define WHITE  0xFFFFFF
 
+int sliceLen = 9;
+int numHSlices = 4;
+int hSlices[4][9] {
+  {0, 7, 8, 11, 12, 18, 19, -1, -1},
+  {2, 5, 6, 14, 15, 16, 17, 20, 21},
+  {1, 9, 10, 13, 22, 23, -1, -1, -1},
+  {3, 4, -1, -1, -1, -1, -1, -1, -1},
+};
+
 int currLED = 0;
+int currSlice = 0;
 
 void loop() {
-  for (int i = 0; i < leds.numPixels(); i++) {
-    if (i == currLED) {
-      if (i % 5 == 0) {
-        leds.setPixel(i, RED);
-      }
-      else {
-        leds.setPixel(i, BLUE);
-      }
-    }
-    else {
-      leds.setPixel(i, GREEN);
+  colorWipe(WHITE);
+  for (int i = 0; i < sliceLen; i++) {
+    int index = hSlices[currSlice][i];
+    if (index >= 0) {
+      leds.setPixel(index, RED);
     }
   }
   leds.show();
   delayMicroseconds(2000000);
 
-  currLED++;
-  if (currLED >= ledsPerStrip) {
-    currLED = 0;
+  currSlice++;
+  if (currSlice >= numHSlices) {
+    currSlice = 0;
   }
 }
 
 void colorWipe(int color) {
   for (int i = 0; i < leds.numPixels(); i++) {
     leds.setPixel(i, color);
-    leds.show();
   }
 }
